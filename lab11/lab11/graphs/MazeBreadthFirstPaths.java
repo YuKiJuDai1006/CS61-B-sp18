1b852121ebc2c97bb1818889553a4b7e3f08ec24
Your Name <you@example.com>
2022-04-30 11:58:23 +0800

    4/30

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(v);
+        marked[v] = true;
+        announce();
+
+        while (!q.isEmpty()) {
+            int temp = q.remove();
+            if (temp == t) {
+                return;
+            }
+            for (int w : maze.adj(temp)) {
+                if (!marked[w]) {
+                    edgeTo[w] = temp;
+                    distTo[w] = distTo[temp] + 1;
+                    marked[w] = true;
+                    q.add(w);
+                    announce();
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,37 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s = 0;
+    private int[] pre;
 
     public MazeCycles(Maze m) {
         super(m);
+        pre = new int[m.V()];
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        solveHelper(s);
     }
 
     // Helper methods go here
+    private void solveHelper(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (marked[w] && pre[w] != v) {
+                announce();
+                return;
+            }
+            if (!marked[w]) {
+                pre[w] = v;
+                announce();
+                solveHelper(w);
+            }
+
+        }
+    }
 }
 
